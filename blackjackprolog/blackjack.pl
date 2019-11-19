6993ef7267688aee03c0894a9ea5ed7f57fc9a77
paulonbc <paulo.carvalho@ccc.ufcg.edu.br>
2019-11-19 16:56:33 -0300

    ...

--- blackjackprolog/blackjack.pl
+++ blackjackprolog/blackjack.pl
@@ -1,4 +1,6 @@
 :- initialization main.
+:- use_module(library(random)).
+:- use_module(library(lists)).
 
 card("Aś",  1).
 card("2",   2).
@@ -18,16 +20,17 @@ cartas(Cartas):-
     Cartas = ["Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
     "Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
     "Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
-    "Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
-%    random_member(X, Cartas),
-%   writeln(X).
+    "Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"].
 
 menu1jogador:-
     writeln('Qual o seu nome jogador?'),
     lerString(Jogador1),
     format('~w se prepare, o jogo vai começar!', [Jogador1]),
-%    cartas(Cartas),
+    writeln('Quer jogar? s/n'),
+    lerString(E),
+    verifica_resp(E, 1, C),
     nl.
+
 
 menu2jogadores:-
     writeln('Qual o nome do primeiro jogador?'), nl,
@@ -35,6 +38,8 @@ menu2jogadores:-
     writeln('Qual o nome do primeiro jogador?'), nl,
     read_line_to_string(user_input, Nome2), nl,
     format('~w e ~w se preparem, o jogo vai começar!', [Nome1, Nome2]), nl.
+    
+
 
 menu_instrucoes:-
     writeln('BlackJack é um jogo que usa um baralho francês de 52 cartas para definir a pontuação.'),
@@ -66,8 +71,62 @@ opcao("3"):-
     menu_instrucoes.
 
 opcao("4"):-
-    writeln('Obrigado por ter jogado BlackJack!'),
+    nl, writeln('Obrigado por ter jogado BlackJack!'), nl,
     halt.
+
+player1(Cartas):-
+    retira_carta(Cartas, E, NewList).
+
+
+player2(Cartas):-
+    retira_carta(Cartas, E, NewList).
+
+
+playerpc(Cartas):-
+retira_carta(Cartas, E, NewList).
+
+verifica_resp("n", 1, 2):-
+    player2(Cards).
+
+verifica_resp("N", 1, 2):-
+    player2().
+
+verifica_resp("n", 1, 2):-
+    player2(Cards).
+
+verifica_resp("Y", 1, 2):-
+    player1().
+
+
+verifica_resp("n", 1, C):-
+
+verifica_resp("N", 1, C):-
+
+verifica_resp("Y", 1, C):-
+
+verifica_resp("n", 1, C):-
+
+verifica_resp("Y", 2, 1):-
+
+verifica_resp("y", 2, 1):-
+
+verifica_resp("N", 2, 1):-
+
+verifica_resp("n", 2, 1):-
+
+
+retira_carta(List, NewList, 1) :-
+    random_member(Element, List),
+    select(Element, List, NewList).
+
+
+retira_carta(List, NewList, 2) :-
+    random_member(Element, List),
+    select(Element, List, NewList).
+
+retira_carta(List, NewList, C) :-
+    random_member(Element, List),
+    select(Element, List, NewList).
 
 lerString(E):- read_line_to_codes(user_input, X), atom_string(X, E).
 
